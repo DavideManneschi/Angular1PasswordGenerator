41a91aae1b7ac0be9ee2bb1c085636db14e6a1c8
Davide Manneschi <dmanneschi@yahoo.it>
2023-06-21 00:27:52 -0400

    finished app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainDashboardPasswordGeneratorComponent } from './main-dashboard-password-generator/main-dashboard-password-generator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainDashboardPasswordGeneratorComponent
   ],
   imports: [
     BrowserModule
